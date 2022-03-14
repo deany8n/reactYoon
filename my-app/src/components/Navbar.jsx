dbee946ab29a8c2167835c367e3bb5f1c2493177
Daniel Yoon <danielyoon28@gmail.com>
2022-03-14 15:31:00 -0300

    cambios en el navbar

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -3,18 +3,17 @@ import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap"
 function NavbarComponent (){
     return <Navbar bg="light" expand="lg">
     <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">KIMCHUSKI</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          <Nav.Link href="#home">Inicio</Nav.Link>
+          <Nav.Link href="#link">Contacto</Nav.Link>
+          <NavDropdown title="Productos" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">KIMCHI</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">SALSAS</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">OTROS FERMENTOS</NavDropdown.Item>
+            
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
